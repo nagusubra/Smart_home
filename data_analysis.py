f583e42b213fa85e836523c80ed4ac6cf90ff2d7
Subramanian Narayanan <Subramanian_Narayanan@transalta.com>
2022-11-27 19:02:30 -0700

    Added pool price data alberta

--- data_analysis.py
+++ data_analysis.py
@@ -7,3 +7,7 @@ raw_data = date_reader.get_raw_data()
 
 
 overal_home = raw_data[['use [kW]', 'gen [kW]', 'House overall [kW]', 'Solar [kW]']]
+
+
+
+last_row = raw_data.iloc[len(raw_data)-5:len(raw_data),:]
