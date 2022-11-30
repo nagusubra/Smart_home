35fca7cdb91104af6ef53392c0e02d9442a67172
Subramanian Narayanan <Subramanian_Narayanan@transalta.com>
2022-11-30 00:02:51 -0700

    Added the Data Analysis for Pool Price with more sample data

--- historical_pool_price_file_generator.py
+++ historical_pool_price_file_generator.py
@@ -14,3 +14,17 @@ pool_price_df["Date (HE)"] = pool_price_df["Date (HE)"].apply(lambda x: str(x)[:
 pool_price_df["Date (HE)"] = pool_price_df["Date (HE)"].apply(lambda x: datetime.strptime(x, "%d/%m/%Y-%H"))
 pool_price_df = pool_price_df.rename(columns = {"Date (HE)": "Datetime", "Price ($)":"Price ($/MWh)"})
 pool_price_df.to_csv(output_folder_path, index=False)
+
+
+
+
+input_folder_path = "data/Hourly_Metered_Volumes_and_Pool_Price_and_AIL.csv"
+output_folder_path = "data/usable_data/Hourly_Pool_Price_Alberta.csv"
+
+
+pool_price_df = pd.read_csv(input_folder_path)
+pool_price_df = pool_price_df[["Date_Begin_GMT","ACTUAL_POOL_PRICE"]]
+#pool_price_df["Date_Begin_GMT"] = pool_price_df["Date_Begin_GMT"].apply(lambda x: str(x)[:1].zfill(2) + "/" + str(x)[3:4].zfill(2) + "/" str(int(str(x)[10:])-1))
+pool_price_df["Date_Begin_GMT"] = pool_price_df["Date_Begin_GMT"].apply(lambda x: datetime.strptime(x, "%m/%d/%Y %H:%M"))
+pool_price_df = pool_price_df.rename(columns = {"Date_Begin_GMT": "Datetime", "ACTUAL_POOL_PRICE":"Price ($/MWh)"})
+pool_price_df.to_csv(output_folder_path, index=False)
